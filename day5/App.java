338a832be1f3c154ebb95bb6b4cd4982035b545b
Matej Skelo <matej@skelo.nl>
2023-12-05 17:55:52 +0100

    got day 5 parser working

--- day5/App.java
+++ day5/App.java
@@ -19,26 +19,40 @@ import java.util.stream.*;
 
 public class App {
     public static void main(String[] args) {
-        List<String> lines = new ArrayList<String>();         
+        List<String> lines = new ArrayList<>();         
+        List<String> seeds = new ArrayList<>();
         Map<Integer, Integer> resultMap = new HashMap<>(); // seed: int, soil: int
 
         // I/O
         try   { lines = Files.readAllLines(Paths.get("./input")); } 
         catch ( IOException e ) { e.printStackTrace(); }
-        // per line: destRange sourceRange length
+        // Parses line values
         for (int i = 0; i < lines.size(); i++) {
-            if (!lines.get(i).chars().anyMatch(Character::isLetter)) {
-                System.out.println(lines.get(i).split(" ")[0]);
+            // parses the line with the seeds
+            if (lines.get(i).chars().anyMatch(Character::isLetter) && 
+                lines.get(i).chars().anyMatch(Character::isDigit)) 
+            {
+                seeds.addAll(Stream.of(lines.get(i).split(":")[1].split(" "))
+                                   .filter(str -> !str.isEmpty())
+                                   .collect(Collectors.toList()));
+            }
+            // the juicy, juicy numbers
+            else if (!lines.get(i).chars().anyMatch(Character::isLetter) &&
+                      lines.get(i).chars().anyMatch(ch -> ch == ' ')) 
+            {
                 long destRange = Long.parseLong(lines.get(i).split(" ")[0]), 
                       srcRange = Long.parseLong(lines.get(i).split(" ")[1]), 
                         length = Long.parseLong(lines.get(i).split(" ")[2]);
                 System.out.println(destRange +" "+srcRange +" "+length);
-            } else {
+            } 
+            // map names or empty lines. maybe i'll need these later
+            else 
+            {
                 continue;
             }
         }
         
-        System.out.println("Advent of Code 2023 // Day 4 // Matej Skelo");
+        System.out.println("Advent of Code 2023 // Day 5 // Matej Skelo");
         System.out.println("Part 1: "/*+part1*/);
         System.out.println("Part 2: " );
     }
