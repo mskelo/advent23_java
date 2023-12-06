69eba6432c7dcd57e04781c189809bbfe1d59060
Matej Skelo <matej@skelo.nl>
2023-12-06 23:01:48 +0100

    one small step for man (day5)

--- day5/App.java
+++ day5/App.java
@@ -22,90 +22,71 @@ public class App {
     static Map<String, String> seedLocation = new HashMap<>();
     public static void main(String[] args) {
         // I/O
-        try   { lines = Files.readAllLines(Paths.get("./input")); } 
+        try   { lines = Files.readAllLines(Paths.get("./calibration")); } 
         catch ( IOException e ) { e.printStackTrace(); }
 
         List<String> tempLines = new ArrayList<>();
-        for (int i = 0; i < lines.size(); i++) {
-            boolean isSeedLine   =  lines.get(i).chars().anyMatch(Character::isLetter) && 
-                                    lines.get(i).chars().anyMatch(Character::isDigit);
-            boolean isMapName    =  lines.get(i).chars().anyMatch(Character::isLetter) && 
-                                   !lines.get(i).chars().anyMatch(Character::isDigit);
-            boolean juicyNumbers = !lines.get(i).chars().anyMatch(Character::isLetter) && 
-                                    lines.get(i).chars().anyMatch(Character::isDigit);
-            boolean isEmpty      =  lines.get(i).length() == 0;
-
-            if (isSeedLine) {
-                Stream.of(lines.get(i).split(":")[1].split(" "))
-                      .filter(str -> !str.isEmpty())
-                      .forEach(seed -> seedLocation.put(seed, ""));
-            }
-            else if (isMapName) {
-                tempLines = new ArrayList<>();
-            }
-            else if (isEmpty) {
+        for (int i = 0; i < lines.size()+1; i++) {
+            System.out.println(i);
+            if (i == lines.size() || lines.get(i).isEmpty()) {
                 parser(tempLines);
             }
+            else {
+                boolean isSeedLine   =  lines.get(i).chars().anyMatch(Character::isLetter) && 
+                                        lines.get(i).chars().anyMatch(Character::isDigit);
+                boolean isMapName    =  lines.get(i).chars().anyMatch(Character::isLetter) && 
+                                       !lines.get(i).chars().anyMatch(Character::isDigit);
+                boolean juicyNumbers = !lines.get(i).chars().anyMatch(Character::isLetter) && 
+                                        lines.get(i).chars().anyMatch(Character::isDigit);
+                if (isMapName) {
+                    tempLines = new ArrayList<>();
+                    System.out.println("Map name: "+ lines.get(i));
+                }
+                else if (isSeedLine) {
+                    Stream.of(lines.get(i).split(":")[1].split(" "))
+                        .filter(str -> !str.isEmpty())
+                        .forEach(seed -> seedLocation.put(seed, "-1"));
+                }
 
-            else if (juicyNumbers) {
-                tempLines.add(lines.get(i));
-            }
+                else if (juicyNumbers) {
+                    tempLines.add(lines.get(i));
+                    System.out.println("line added "+ lines.get(i));
+                }
+            }    
+            if (i == lines.size()) {
+                parser(tempLines);
+            }    
         }
+        System.out.println("End: "+seedLocation);
         System.out.println("Advent of Code 2023 // Day 5 // Matej Skelo");
         System.out.println("Part 1: " + Collections.min(seedLocation.values()));
         System.out.println("Part 2: " );
     }
 
     public static void parser(List<String> lines) {
-        System.out.println();
+        System.out.println("top of parser(): "+seedLocation);
         for (String seed : seedLocation.keySet()) {
             for (int i = 0; i < lines.size(); i++) {
                 System.out.println("\nSeed: "+seed);
+                System.out.println(seedLocation.values());
                 long destRange = Long.parseLong(lines.get(i).split(" ")[0]),
                      srcRange  = Long.parseLong(lines.get(i).split(" ")[1]),
                      length    = Long.parseLong(lines.get(i).split(" ")[2]);
                 System.out.println(destRange +" "+srcRange +" "+length);
-                
-                boolean seedIsUnmapped  = seedLocation.get(seed).equals("");
-                boolean seedInDestRange = Long.parseLong(seedLocation.get(seed)) >= destRange && Long.parseLong(seedLocation.get(seed)) < destRange + length;
-                boolean seedInSrcRange  = Long.parseLong(seedLocation.get(seed)) >= srcRange && Long.parseLong(seedLocation.get(seed)) < srcRange + length;
-                
-                // This needs to be fixed
+                Long val = Long.parseLong(seedLocation.get(seed));
+                boolean seedIsUnmapped  = seedLocation.get(seed).equals("-1");
                 if (seedIsUnmapped) {
                     seedLocation.put(seed, seed);
                     System.out.println(seedLocation.get(seed));
                 }
-                else if (seedInDestRange) {
-                    String newValue = Long.toString(Long.parseLong(seedLocation.get(seed))-destRange+srcRange);
-                    seedLocation.put(seed, newValue);
-                    System.out.println("in destRange: "+seedLocation.get(seed));
-                } 
-                else if (seedInSrcRange) {
-                    String newValue = Long.toString(Long.parseLong(seedLocation.get(seed))-srcRange+destRange);
-                    seedLocation.put(seed, newValue);
-                    System.out.println("in srcRange: "+seedLocation.get(seed));
-                } 
-                 
+                else if (srcRange <= val && val < srcRange + length) {
+                    Long newValue = (val - srcRange) + destRange;
+                    seedLocation.put(seed, newValue.toString());
+                    System.out.println("seed: "+seed+"newValue: "+newValue);
+                }
             }
         }
     }
 
 
 }
-
-
-
-// for line in lines
-    // if line ONLY has letters and numbers
-        // put every line in a List until you hit an empty line
-        // push List to parser
-// print seeds.values().min()
-
-// parser(List lines)
-    // for seed in seed.keySet()
-        // for line in lines
-            // if seed >= destNr && seed < destNr + length
-                //seeds.seed = seed-dstNr+srcNr
-            // else it's unmapped and keeps its value
-
-
