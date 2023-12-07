c1cba3a0fecefd4a269d0f1b565a98c529d2f9cb
Matej Skelo <matej@skelo.nl>
2023-12-07 22:02:12 +0100

    tiny little equals sign fixed everything lmao

--- day5/App.java
+++ day5/App.java
@@ -28,9 +28,6 @@ public class App {
 
         List<String> tempLines = new ArrayList<>();
         for (String line : lines) {
-            if (line.isEmpty()) {
-                System.out.println("Empty: "+line.length());
-            }
             if (line.isEmpty() && tempLines.size() > 0) {
                 parser(tempLines);
             }
@@ -41,46 +38,34 @@ public class App {
                                        !line.chars().anyMatch(Character::isDigit);
                 boolean juicyNumbers = !line.chars().anyMatch(Character::isLetter) && 
                                         line.chars().anyMatch(Character::isDigit);
-                if (isMapName) {
-                    tempLines = new ArrayList<>();
-                    System.out.println("Map name: "+ line);
-                }
-                else if (isSeedLine) {
-                    Stream.of(line.split(":")[1].split(" "))
-                        .filter(str -> !str.isEmpty())
-                        .forEach(seed -> seedLocation.put(seed, seed));
-                }
-
-                else if (juicyNumbers) {
-                    tempLines.add(line);
-                    System.out.println("line added "+ line);
-                }
+                
+                if      (isMapName)    { tempLines = new ArrayList<>(); }
+                else if (juicyNumbers) { tempLines.add(line); }
+                else if (isSeedLine)   { Stream.of(line.split(":")[1].split(" "))
+                                               .filter(str -> !str.isEmpty())
+                                               .forEach(seed -> seedLocation.put(seed, seed)); }
             }    
         }
-        System.out.println("End: "+seedLocation);
+        long part1 = seedLocation.values().stream()
+                                          .map(Long::parseLong)
+                                          .min(Comparator.naturalOrder())
+                                          .get();
         System.out.println("Advent of Code 2023 // Day 5 // Matej Skelo");
-        System.out.println("Part 1: " + seedLocation.values().stream().map(Long::parseLong).min(Comparator.naturalOrder()).get());
-        // System.out.println("Part 1: " + Collections.min(seedLocation.values()));
-        // This actually made me /facedesk. min(String) =/= min(Long)
+        System.out.println("Part 1: " + part1);
         System.out.println("Part 2: " );
     }
 
     public static void parser(List<String> lines) {
-        System.out.println("top of parser(): "+seedLocation);
         for (String seed : seedLocation.keySet()) {
             for (String line : lines) {
-                System.out.println("\nSeed: "+seed);
                 Long destRange = Long.parseLong(line.split(" ")[0]),
                      srcRange  = Long.parseLong(line.split(" ")[1]),
                      length    = Long.parseLong(line.split(" ")[2]);
-                System.out.println(destRange +" "+srcRange +" "+length);
                 Long seedVal = Long.parseLong(seedLocation.get(seed));
-
-                if (seedVal >= srcRange && seedVal < srcRange + length) {
+                if (seedVal >= srcRange && seedVal <= srcRange + length) {
+                                        //          ^     this ended up being the issue all along lmao
                     Long newValue = (seedVal - srcRange) + destRange;
-                    Long oldValue = Long.parseLong(seedLocation.get(seed));
                     seedLocation.put(seed, newValue.toString());
-                    System.out.println("seed: "+seed+", oldValue: "+oldValue+", newValue: "+newValue);
                     break; // Holy shit. This line ended two days of suffering.
                     // You don't need to keep looking once you've matched your seed's number
                     // Don't forget about context and what the values you work with represent
