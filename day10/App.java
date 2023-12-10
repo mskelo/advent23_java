78885abc15eee3cd8221cbcf69134f54561f272a
Matej Skelo <matej@skelo.nl>
2023-12-10 21:59:36 +0100

    day 10 start

---
+++ day10/App.java
@@ -0,0 +1,33 @@
+/**
+ * Advent of Code 2023
+ * Day 10, Part 1
+ */
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.LinkedHashMap;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class App {
+    public static void main(String[] args) {
+        // I/O
+        List<String> lines = new ArrayList<>();
+        try   { lines = Files.readAllLines(Paths.get("./calibration")); } 
+        catch ( IOException e ) { e.printStackTrace(); }
+
+        // Parser
+        for (String line : lines) {
+            
+        }  
+        System.out.println("Advent of Code 2023 // Day 10 // Matej Skelo");
+        System.out.println("Part 1: " );
+        System.out.println("Part 2: " );
+    }
+}
+
