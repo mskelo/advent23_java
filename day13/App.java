b43203f0f58a316e246f41ff6a5df05b49a7fa02
Matej Skelo <matej@skelo.nl>
2023-12-17 14:18:58 +0100

    cleaned up main()

--- day13/App.java
+++ day13/App.java
@@ -12,38 +12,21 @@ import java.util.stream.IntStream;
 import java.util.ArrayList;
 
 public class App {
-    public static void main(String[] args) {
-        List<String> lines = new ArrayList<>();
-        // I/O
-        try   { lines = Files.readAllLines(Paths.get("./calibration")); } 
-        catch ( IOException e ) { e.printStackTrace(); }
-        
+    public static List<List<String>> parseParagraphs(List<String> lines) {
         List<List<String>> paragraphs = new ArrayList<>();
         List<String> temp = new ArrayList<>();
         for (String line : lines) {
-            if (!line.isBlank()) 
-                temp.add(line);
+            if   (!line.isBlank()) temp.add(line);
             else {
                 paragraphs.add(temp); 
                 temp = new ArrayList<>(); 
             }
         }
-
-        for (List<String> paragraph : paragraphs) {
-            // 100 * (rows above mirror)
-            //     + (columns before mirror)
-
-            int verticalMirror = findMirror(0, transpose(paragraph));
-            int horizontalMirror = findMirror(0, paragraph);
-            if (verticalMirror != -1) System.out.println(verticalMirror);
-            else System.out.println(horizontalMirror);
-        }
-
-        System.out.println("Advent of Code 2023 // Day 13 // Matej Skelo");
-        System.out.println("Part 1: " );
-        System.out.println("Part 2: " );
+        return paragraphs;
     }
 
+    // Transposes a paragraph (matrix) by 90 degrees to find vertical mirrors
+    // Because it's easier to check horizontal lines for equality
     public static List<String> transpose(List<String> lines) {
         return IntStream.range(0, lines.size())
                         .mapToObj(i -> lines.stream()
@@ -53,6 +36,10 @@ public class App {
         /* --> return */.collect(Collectors.toList());
     }
 
+    // Only finds mirrors in horizontal lines
+    // Transpose a matrix first to find vertical mirrors
+    // With recursion, 'cuz y nut. 
+    // Returns -1 if no mirrors are found. Else first index of repeating lines.
     public static int findMirror(int index, List<String> lines) {
         if (index == lines.size()-1) return -1;
         int indexUp = index, 
@@ -64,8 +51,30 @@ public class App {
                 if (!lines.get(indexDn).equals(lines.get(indexUp))) {isMirror = false; break;}
                 indexDn--;
             }
-            return isMirror ? index : -1;
+            return isMirror ? index : -1; // Here's the final return
         }
         else return findMirror(++index, lines);
+    }
+
+    public static void main(String[] args) {
+        List<String> lines = new ArrayList<>();
+        // I/O
+        try   { lines = Files.readAllLines(Paths.get("./calibration")); } 
+        catch ( IOException e ) { e.printStackTrace(); }
+        
+        List<List<String>> paragraphs = parseParagraphs(lines);
+
+        for (List<String> paragraph : paragraphs) {
+            // 100 * (rows above mirror)
+            //     + (columns before mirror)
+
+            int verticalMirror = findMirror(0, transpose(paragraph));
+            int horizontalMirror = findMirror(0, paragraph);
+            if (verticalMirror != -1) System.out.println(verticalMirror);
+            else System.out.println(horizontalMirror);
+        }
+        System.out.println("Advent of Code 2023 // Day 13 // Matej Skelo");
+        System.out.println("Part 1: " );
+        System.out.println("Part 2: " );
     }
 }
